401ae0f97e0c6729d9864bcff7960eabed1d6e27
frogfather <buyjohn@gmail.com>
2018-04-30 18:21:36 +0100

    * ExcelReader.cs: Excel Reader added for completeness but can't be   used in this context
    * FileUtilities.cs: File utilities static class added
    
    * LPReportCheck.csproj: Modifications due to adding new classes

---
+++ LPReportCheck/ExcelReader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ReportChecker
+{
+    class ExcelReader
+    {
+        Excel.Application xlApp;
+
+        public ExcelReader(string filename)
+        {
+            xlApp = new Excel.Application();
+            xlWorkbook = xlApp.Workbooks.Open(filename);
+            SetPage(1);
+        }
+
+        public Excel.Range xlRange { get; set; }
+
+        public Excel.Workbook xlWorkbook { get; set; }
+
+        public Excel._Worksheet xlWorksheet { get; set; }
+
+        public void SetPage(int sheetNo)
+
+        {
+            xlWorksheet = xlWorkbook.Sheets[sheetNo];
+            xlRange = xlWorksheet.UsedRange;
+        }
+
+        public string GetCell(int row, int col)
+        {
+            if (xlRange.Cells[row, col] != null && xlRange.Cells[row, col].Value2 != null)
+            {
+                return xlRange.Cells[row, col].Value2.ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        ~ExcelReader()
+        {
+            //You have to release references to these objects manually otherwise file access writes to the workbook will persist.
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //release com objects
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        }
+    }
+}
---
+++ LPReportCheck/FileUtilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LPReportCheck
+{
+    public static class FileUtilities
+    {
+        private static String GetDirectoryFromFileName(string fileName)
+        {
+            return fileName.Substring(0, fileName.LastIndexOf("\\",StringComparison.Ordinal) + 1);
+        }
+
+        private static DateTime GetFileDate(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                return File.GetCreationTime(fileName);
+            }
+            else
+            {
+                return Convert.ToDateTime("11/11/2222");
+            }
+
+        }
+    }
+}
